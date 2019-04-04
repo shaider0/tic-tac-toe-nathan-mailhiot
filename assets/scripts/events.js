3397baef948bff223dee7a550de3afc245ae5fa2
nathanmailhiot <nathanjm12@outlook.com>
2019-04-04 12:02:03 -0400

    Add game logic Add winner's Add tie

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -13,7 +13,10 @@ let currentPlayer = 'x'
 //  empty game board
 const ticBoard = ['', '', '', '', '', '', '', '', '']
 
-// whose turn
+// check if game is a tie or gameOver
+let gameOver = false
+
+// turn
 const turn = () => {
   if (currentPlayer === 'x') {
     currentPlayer = 'o'
@@ -23,47 +26,46 @@ const turn = () => {
   return currentPlayer
 }
 
-// winner logic
-const winner = function (ticBoard) {
+// win statements
+const gameState = function () {
   if (
-    ((ticBoard[0] === ticBoard[1]) && (ticBoard[1] === ticBoard[2])) ||
-    ((ticBoard[3] === ticBoard[4]) && (ticBoard[4] === ticBoard[5])) ||
-    ((ticBoard[6] === ticBoard[7]) && (ticBoard[7] === ticBoard[8])) ||
-    ((ticBoard[0] === ticBoard[4]) && (ticBoard[4] === ticBoard[8])) ||
-    ((ticBoard[2] === ticBoard[4]) && (ticBoard[4] === ticBoard[6])) ||
-    ((ticBoard[0] === ticBoard[3]) && (ticBoard[3] === ticBoard[6])) ||
-    ((ticBoard[1] === ticBoard[4]) && (ticBoard[4] === ticBoard[7])) ||
-    ((ticBoard[2] === ticBoard[5]) && (ticBoard[5] === ticBoard[8]))) {
-    alert(`${currentPlayer}  you won!`)
+    (ticBoard[0] === ticBoard[1] && ticBoard[1] === ticBoard[2] && ticBoard[0] === currentPlayer) ||
+    (ticBoard[3] === ticBoard[4] && ticBoard[4] === ticBoard[5] && ticBoard[3] === currentPlayer) ||
+    (ticBoard[6] === ticBoard[7] && ticBoard[7] === ticBoard[8] && ticBoard[6] === currentPlayer) ||
+    (ticBoard[0] === ticBoard[4] && ticBoard[4] === ticBoard[8] && ticBoard[0] === currentPlayer) ||
+    (ticBoard[2] === ticBoard[4] && ticBoard[4] === ticBoard[6] && ticBoard[2] === currentPlayer) ||
+    (ticBoard[0] === ticBoard[3] && ticBoard[3] === ticBoard[6] && ticBoard[0] === currentPlayer) ||
+    (ticBoard[1] === ticBoard[4] && ticBoard[4] === ticBoard[7] && ticBoard[2] === currentPlayer) ||
+    (ticBoard[2] === ticBoard[5] && ticBoard[5] === ticBoard[8] && ticBoard[2]) === currentPlayer) {
+    gameOver = true
+    console.log('winner')
+  } else if (ticBoard.every(index => index !== '')) {
+    gameOver = true
+    console.log('It is a tie')
   }
 }
 
-const onClickbox = function () {
+// onClickbox
+const onClickbox = function (event) {
   const currentSquare = $(event.target).data('id')
   const content = $(event.target).text()
-  // if data id empty allow player to add text
-  $(event.target).text(currentPlayer)
-  console.log('click', currentSquare)
-  ticBoard[currentSquare] = currentPlayer
+  console.log('click', currentPlayer)
   if (content === '' && currentPlayer === 'x') {
     $(event.target).text(currentPlayer)
-    winner(ticBoard)
+    ticBoard[currentSquare] = currentPlayer
+    gameState(ticBoard, currentPlayer)
     turn()
   } else if (content === '' && currentPlayer === 'o') {
     $(event.target).text(currentPlayer)
+    ticBoard[currentSquare] = currentPlayer
+    gameState(ticBoard, currentPlayer)
     turn()
-  } else {
-    console.log('taken')
   }
-
-  console.log(ticBoard)
-  console.log(currentPlayer)
 }
+
 const addEventHandler = function () {
   $('.square').on('click', onClickbox)
 }
-
-
 
 module.exports = {
   addEventHandler
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,8 +1,23 @@
+
+const events = require('./events.js')
 // change the board with x and o
-const clickSuccess = function (event) {
-$(event.target).text(currentPlayer)
+// winner logic
+const winner = function () {
+  if (
+    ((ticBoard[0] === ticBoard[1]) && (ticBoard[1] === ticBoard[2])) ||
+    ((ticBoard[3] === ticBoard[4]) && (ticBoard[4] === ticBoard[5])) ||
+    ((ticBoard[6] === ticBoard[7]) && (ticBoard[7] === ticBoard[8])) ||
+    ((ticBoard[0] === ticBoard[4]) && (ticBoard[4] === ticBoard[8])) ||
+    ((ticBoard[2] === ticBoard[4]) && (ticBoard[4] === ticBoard[6])) ||
+    ((ticBoard[0] === ticBoard[3]) && (ticBoard[3] === ticBoard[6])) ||
+    ((ticBoard[1] === ticBoard[4]) && (ticBoard[4] === ticBoard[7])) ||
+    ((ticBoard[2] === ticBoard[5]) && (ticBoard[5] === ticBoard[8]))) {
+    alert(`${currentPlayer}  you won!`)
+  }
 }
 
-const clickFailure = function (event) {
-
+if (ticBoard.lastIndexOf('') !== undefined) {
+  winner()
+} else {
+  console.log('it is a tie')
 }
